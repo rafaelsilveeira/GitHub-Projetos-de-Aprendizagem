8fe5eb219af071c4ea0e62d8c02a86ce02159199
Rafael S. da Rosa <rafael_sr30@hotmail.com>
2020-05-05 23:52:27 -0300

    Atualização do certificado

--- Gerador_de_Certificado_Alura/script.js
+++ Gerador_de_Certificado_Alura/script.js
@@ -5,20 +5,22 @@ function gerar(){
     
     listaLinks = [
         'https://codepen.io/rafaelsilveeira/full/KKdPgOQ', 
-        'https://codepen.io/rafaelsilveeira/full/LYpRYJJo.js', 
-        'https://codepen.io/rafaelsilveeira/full/JjYyvEY'
+        'https://codepen.io/rafaelsilveeira/full/LYpRYJJ', 
+        'https://codepen.io/rafaelsilveeira/full/JjYyvEY',
+        'https://codepen.io/rafaelsilveeira/full/vYNZwev'
     ];
     atividade = [
         'Calculadora de tarifa para app de mobilidade',
         'Calculadora de IMC',
-        'Sorteador de número aleatório'
+        'Sorteador de número aleatório',
+        'Botões de like - Face e Instagram'
     ];
     
     listalinkscomhtml = "";
     for(cont=0;cont<listaLinks.length;cont++){
         let link = listaLinks[cont];
         let projeto = atividade[cont];
-        listalinkscomhtml += `<li><a href = "${link}"> ${projeto}</a></li>`;
+        listalinkscomhtml += `<li><a href = "${link}" target='blank'> ${projeto}</a></li>`;
     }
 
     document.body.innerHTML = `
