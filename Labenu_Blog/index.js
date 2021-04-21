f5799516b91c56d4b8e4695c207f8a4f5ad03f39
Rafael S. da Rosa <rafaelsilveeira@users.noreply.github.com>
2021-04-21 14:01:35 -0300

    campo de imagem adicionado

--- Labenu_Blog/index.js
+++ Labenu_Blog/index.js
@@ -2,11 +2,11 @@
 const listaDePosts = [];
 
 
-function cadastrarPosts(){
+function cadastrarPosts() {
     let cadastro = document.getElementsByTagName('body')
-    
-    cadastro[0].innerHTML = 
-    `
+
+    cadastro[0].innerHTML =
+        `
     <button onclick="cadastrarPosts()">Cadastrar Post</button>
     <button onclick="verPosts()">Ver Posts</button>
     
@@ -19,24 +19,27 @@ function cadastrarPosts(){
 
         <label for="conteudo-post">Conteúdo:</label>
         <textarea id="conteudo-post" cols="30" rows="10" maxlength="500"></textarea>
+
+        <label for="imagem-post">Link da imagem destacada:</label>
+        <input type="text" id="imagem-post">
     
         <button onclick="incluirPost()">Criar Post</button>
     </section>
     `
 
-console.log(cadastro);
+    console.log(cadastro);
 }
 
-function verPosts(){
+function verPosts() {
 
-    if (listaDePosts.length === 0){
+    if (listaDePosts.length === 0) {
         alert("Ainda não existem posts cadastrados.")
     }
 
     let blog = document.getElementsByTagName('body')
-    
-    blog[0].innerHTML = 
-    `
+
+    blog[0].innerHTML =
+        `
     <button onclick="cadastrarPosts()">Cadastrar Post</button>
     <button onclick="verPosts()">Ver Posts</button>
     
@@ -52,24 +55,41 @@ function incluirPost() {
     const tituloDoPost = document.getElementById('titulo-post').value
     const autorDoPost = document.getElementById('autor-post').value
     const conteudoDoPost = document.getElementById('conteudo-post').value
+    const imagemDoPost = verificaImagem(document.getElementById('imagem-post').value)
 
-    if (tituloDoPost.length > 0 && autorDoPost.length > 0 && conteudoDoPost.length > 0) {
-        postAdicionado = capturarEntradasUsuario(tituloDoPost, autorDoPost, conteudoDoPost) //retorna objeto
-        listaDePosts.push(postAdicionado);
-        
-        limparInputs()
-    } else {
-        alert('Por favor, preencha todos os campos');
+    if (imagemDoPost === false) {
+        alert('Link de imagem inválido');
         return
+    } else {
+        if (tituloDoPost.length > 0 && autorDoPost.length > 0 && conteudoDoPost.length > 0) {
+    
+            postAdicionado = capturarEntradasUsuario(tituloDoPost, autorDoPost, conteudoDoPost, imagemDoPost) //retorna objeto
+            listaDePosts.push(postAdicionado);
+            limparInputs()
+    
+        } else {
+            alert('Por favor, preencha todos os campos');
+            return
+        }
     }
+
     console.log(listaDePosts)
 }
 
-function capturarEntradasUsuario(titulo, autor, conteudo) {
+function verificaImagem(linkImagem) {
+    if (linkImagem.includes('http') && (linkImagem.toLowerCase().includes('.jpg') || linkImagem.toLowerCase().includes('.jpeg') || linkImagem.toLowerCase().includes('.png'))) {
+        return linkImagem
+    } else {
+        return false
+    }
+}
+
+function capturarEntradasUsuario(titulo, autor, conteudo, imagem) {
     const post = {
         titulo: titulo,
         autor: autor,
-        conteudo: conteudo
+        conteudo: conteudo,
+        imagem: imagem
     }
     return post
 }
@@ -78,12 +98,21 @@ function limparInputs() {
     document.getElementById('titulo-post').value = ""
     document.getElementById('autor-post').value = ""
     document.getElementById('conteudo-post').value = ""
+    document.getElementById('imagem-post').value = ""
 }
 
-function publicarBlog(){
+function publicarBlog() {
     let containerDePosts = document.getElementById('container-de-posts')
 
-    for (post in listaDePosts){
-        containerDePosts.innerHTML += `<div class="card-post"> <h1>${listaDePosts[post].titulo}</h1> <h5>${listaDePosts[post].autor}</h5> <p>${listaDePosts[post].conteudo}</p> </div>`
+    for (post in listaDePosts) {
+        containerDePosts.innerHTML +=
+            `
+        <div class="card-post"> 
+        <img src="${listaDePosts[post].imagem}">
+        <h1>${listaDePosts[post].titulo}</h1>
+        <h5>${listaDePosts[post].autor}</h5>
+        <p>${listaDePosts[post].conteudo}</p> 
+        </div>
+        `
     }
 }
